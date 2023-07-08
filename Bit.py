a6745acf21df68f436f2f37e50f7ac1f0d88f953
Tri Huynh <22521519@gm.uit.edu.vn>
2023-07-09 00:08:37 +0700

    NT

---
+++ Bit.py
@@ -0,0 +1,32 @@
+from token import PLUS
+
+
+def PlusOne(a):
+    if (a == "X++" or a == "++X"):
+        return True
+    else:
+        return False
+
+def MinusOne(a):
+    if(a == "X--" or a == "--X"):
+        return True
+    else:
+        return False
+
+def main():
+    n = int(input())
+    count=int(0)
+    for i in range(n):
+        order = input()
+        if (PlusOne(order)):
+            count += 1
+        else:
+           if(MinusOne(order)):
+               count -=1
+    print(count)
+
+if __name__ == "__main__":
+    main()
+
+
+
